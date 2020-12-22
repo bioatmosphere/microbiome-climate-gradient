10b6f2be8d8e8f2e71b20b5922793baef6addd51
bioatmosphere <wbwenwu@gmail.com>
2020-12-21 22:41:22 -0800

    R script mapping site location updated

--- inputs/site_information/site_location.R
+++ inputs/site_information/site_location.R
@@ -11,14 +11,11 @@ library(ggmap)
 
 #downloading the raster:
 MapImage <- get_map(location = c(left=-118.00, bottom=33.0, right=-116.0, top=34.0),
-                         color = "color",
-                         source = "google",
-                         maptype = "terrain",
-                         zoom = 8)
+                    zoom=11, color = "color",source="stamen",maptype = "terrain", force=TRUE)
 
 #site locations
-Longitude = c(-116.38,-116.45,-117.70,-116.77,-116.75)
-Latitude  = c(33.648,33.610,33.737,33.683, 33.823)
+Longitude = c(-116.38, -116.45, -117.70, -116.77, -116.75)
+Latitude  = c(33.648,  33.610,  33.737,  33.683,  33.823)
 data = data.frame(Longitude, Latitude)
 
 #flux tower sites
@@ -31,10 +28,9 @@ data_tower = data.frame(longitude,latitude)
 ggmap(MapImage) +
   geom_point(aes(x = longitude, y = latitude), data = data_tower, alpha = .5, color="blue", size = 3) +
   geom_point(aes(x = Longitude, y = Latitude), data = data, alpha = .5, color="red", size = 3) +
-  annotate('text', x=-116.75, y=33.85, label = 'Subalpine', color = "black", size = 8) +
-  annotate('text', x=-116.77, y=33.68, label = 'Pine-Oak', color = "black", size = 8) +
-  annotate('text', x=-116.45, y=33.58, label = 'Scrubland', color = "black", size = 8) +
-  annotate('text', x=-116.38, y=33.70, label = 'Desert', color = "black", size = 8)
-  
-  
-  
+  annotate('text', x=-116.75, y=33.85, label = 'Subalpine', color = "black", size = 6) +
+  annotate('text', x=-116.77, y=33.68, label = 'Pine-Oak',  color = "black", size = 6) +
+  annotate('text', x=-116.45, y=33.58, label = 'Scrubland', color = "black", size = 6) +
+  annotate('text', x=-116.38, y=33.70, label = 'Desert',    color = "black", size = 6) +
+  annotate('text', x=-117.70, y=33.80, label = 'Grassland', color = "black", size = 6)
+
