f17e6c9d8d8ed96eef9aed143af13632d61da78e
bioatmosphere <wbwenwu@gmail.com>
2022-03-12 13:55:31 -0500

    deposit all 4 accepted pub

--- inputs/automation/inputs_hpc.sh
+++ inputs/automation/inputs_hpc.sh
@@ -5,7 +5,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #file to be transferred
-file="parameters"."csv"
+file="runtime"."txt"
 
 for site in desert scrubland grassland pineoak subalpine
 do
@@ -14,17 +14,19 @@ do
     localfolder="$site"
     # remote folder
     folder="input"_"$localfolder"
+    folder2015="$localfolder"_"2015"
 
     #path="/home/wangb/dementpy"
-    #path="lustre"/"or-scratch"/"cades-ccsi"/"wangb/dementpy
+    #path="/lustre/or-scratch/cades-ccsi/wangb/mic_gradient_dis"
 
-    #base site
-    scp ../$localfolder/$file wangb@or-slurm-login.ornl.gov:/home/wangb/dementpy/$folder
-    #other sites
-    scp ../desert/$file    wangb@or-slurm-login.ornl.gov:/home/wangb/dementpy/$folder/desert
-    scp ../scrubland/$file wangb@or-slurm-login.ornl.gov:/home/wangb/dementpy/$folder/scrubland
-    scp ../grassland/$file wangb@or-slurm-login.ornl.gov:/home/wangb/dementpy/$folder/grassland
-    scp ../pineoak/$file   wangb@or-slurm-login.ornl.gov:/home/wangb/dementpy/$folder/pineoak
-    scp ../subalpine/$file wangb@or-slurm-login.ornl.gov:/home/wangb/dementpy/$folder/subalpine
+    ##base site
+    scp ../$localfolder/$file wangb@or-slurm-login.ornl.gov:/lustre/or-scratch/cades-ccsi/wangb/mic_gradient/$folder
+    #scp ../$localfolder/$file wangb@or-slurm-login.ornl.gov:/lustre/or-scratch/cades-ccsi/wangb/mic_gradient/$folder/$folder2015
+    ##other sites
+    scp ../desert/$file    wangb@or-slurm-login.ornl.gov:/lustre/or-scratch/cades-ccsi/wangb/mic_gradient/$folder/desert
+    scp ../scrubland/$file wangb@or-slurm-login.ornl.gov:/lustre/or-scratch/cades-ccsi/wangb/mic_gradient/$folder/scrubland
+    scp ../grassland/$file wangb@or-slurm-login.ornl.gov:/lustre/or-scratch/cades-ccsi/wangb/mic_gradient/$folder/grassland
+    scp ../pineoak/$file   wangb@or-slurm-login.ornl.gov:/lustre/or-scratch/cades-ccsi/wangb/mic_gradient/$folder/pineoak
+    scp ../subalpine/$file wangb@or-slurm-login.ornl.gov:/lustre/or-scratch/cades-ccsi/wangb/mic_gradient/$folder/subalpine
 
 done
